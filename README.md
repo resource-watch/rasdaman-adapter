1bd13955c07b94cb021345e235117cea049b225a
Enrique Cornejo <enrique@cornejo.me>
2017-06-12 16:39:50 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rasdaman adapter microservice
+
+This repository is an adapter to the Rasdaman multidimensional array Database.
---
+++ app/src/routes/api/v1/service.router.js
@@ -0,0 +1,20 @@
+const Router = require('koa-router');
+
+
+const router = new Router({
+    prefix: '/service'
+});
+
+class Service {
+
+    static sayHi(ctx) {
+        ctx.body = {
+            greeting: 'hi'
+        };
+    }
+
+}
+
+router.get('/hi', Service.sayHi);
+
+module.exports = router;
