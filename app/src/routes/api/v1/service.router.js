e4fe0140eaf6852a54fae9304d7ddcc9bc12a8ff
Enrique Cornejo <enrique@cornejo.me>
2017-06-19 19:09:07 +0200

    Added endpoint for registering datasets

--- app/src/routes/api/v1/service.router.js
+++ app/src/routes/api/v1/service.router.js
@@ -2,10 +2,10 @@ const Router = require('koa-router');
 
 
 const router = new Router({
-    prefix: '/service'
+    prefix: '/rasdaman'
 });
 
-class Service {
+class RasdamanRouter {
 
     static sayHi(ctx) {
         ctx.body = {
@@ -13,8 +13,38 @@ class Service {
         };
     }
 
+    static async registerDataset(ctx) {
+        logger.info('Registering dataset with data', ctx.request.body);
+        try {
+            await RasdamanService.getFields(ctx.request.body.connector.connector_url);
+            await ctRegisterMicroservice.requestToMicroservice({
+                method: 'PATCH',
+                uri: `/dataset/${ctx.request.body.connector.id}`,
+                body: {
+                    dataset: {
+                        status: 1
+                    }
+                },
+                json: true
+            });
+        } catch (e) {
+            await ctRegisterMicroservice.requestToMicroservice({
+                method: 'PATCH',
+                uri: `/dataset/${ctx.request.body.connector.id}`,
+                body: {
+                    dataset: {
+                        status: 2,
+                        errorMessage: `${e.name} - ${e.message}`
+                    }
+                },
+                json: true
+            });
+        }
+        ctx.body = {};
+    }
 }
 
-router.get('/hi', Service.sayHi);
+router.get('/hi', RasdamanRouter.sayHi);
+router.post('/rest-datasets/rasdaman', RasdamanRouter.registerDataset);
 
 module.exports = router;
